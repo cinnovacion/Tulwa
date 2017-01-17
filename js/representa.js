09ea2e26059b1fb5ae3973649307805b17a78e02
Lester Mendoza <lestermt94@gmail.com>
2017-01-17 16:13:03 -0600

    representa-level

---
+++ js/representa.js
@@ -0,0 +1,260 @@
+define(function (require) {
+    return [
+
+        {
+            'serie': [null, 2],
+            'img': 'img/Nivel1/ejer1.png',
+            'op1': 1,
+        },
+
+        {
+            'serie': [null, 4],
+            'img': 'img/Nivel1/ejer2.png',
+            'op1': 2,
+        },
+
+        {
+            'serie': [null, 8],
+            'img': 'img/Nivel1/ejer3.png',
+            'op1': 3,
+        },
+
+        {
+            'serie': [null, 6],
+            'img': 'img/Nivel1/ejer4.png',
+            'op1': 4,
+        },
+        {
+            'serie': [null, 4],
+            'img': 'img/Nivel1/ejer5.png',
+            'op1': 3,
+        },
+        {
+            'serie': [null, 7],
+            'img': 'img/Nivel1/ejer6.png',
+            'op1': 5,
+        },
+        {
+            'serie': [null, 8],
+            'img': 'img/Nivel1/ejer7.png',
+            'op1': 6,
+        },
+        {
+            'serie': [null, 5],
+            'img': 'img/Nivel1/ejer8.png',
+            'op1': 3,
+        },
+        {
+            'serie': [null, 9],
+            'img': 'img/Nivel1/ejer9.png',
+            'op1': 5,
+        },
+        {
+            'serie': [null, 10],
+            'img': 'img/Nivel1/ejer10.png',
+            'op1': 7,
+        },
+        {
+            'serie': [null, 9],
+            'img': 'img/Nivel1/ejer11.png',
+            'op1': 7,
+        },
+        {
+            'serie': [null, 18],
+            'img': 'img/Nivel1/ejer12.png',
+            'op1': 13,
+        },
+        {
+            'serie': [null, 24],
+            'img': 'img/Nivel1/ejer13.png',
+            'op1': 20,
+        },
+        {
+            'serie': [null, 15],
+            'img': 'img/Nivel1/ejer14.png',
+            'op1': 8,
+        },
+        {
+            'serie': [null, 12],
+            'img': 'img/Nivel1/ejer15.png',
+            'op1': 9,
+        },
+        {
+            'serie': [null, 20],
+            'img': 'img/Nivel1/ejer16.png',
+            'op1': 11,
+        },
+        {
+            'serie': [null, 8],
+            'img': 'img/Nivel1/ejer17.png',
+            'op1': 7,
+        },
+        {
+            'serie': [null, 9],
+            'img': 'img/Nivel1/ejer18.png',
+            'op1': 6,
+        },
+        {
+            'serie': [null, 9],
+            'img': 'img/Nivel1/ejer19.png',
+            'op1': 4,
+        },
+        {
+            'serie': [null, 12],
+            'img': 'img/Nivel1/ejer20.png',
+            'op1': 5,
+        },
+        {
+            'serie': [null, 8],
+            'img': 'img/Nivel1/ejer21.png',
+            'op1': 5,
+        },
+        {
+            'serie': [null, 8],
+            'img': 'img/Nivel1/ejer22.png',
+            'op1': 3,
+        },
+        {
+            'serie': [null, 5],
+            'img': 'img/Nivel1/ejer23.png',
+            'op1': 4,
+        },
+        {
+            'serie': [null, 12],
+            'img': 'img/Nivel1/ejer24.png',
+            'op1': 4,
+        },
+        {
+            'serie': [null, 12],
+            'img': 'img/Nivel1/ejer25.png',
+            'op1': 12,
+        },
+        {
+            'serie': [null, 10],
+            'img': 'img/Nivel1/ejer26.png',
+            'op1': 6,
+        },
+        {
+            'serie': [null, 10],
+            'img': 'img/Nivel1/ejer27.png',
+            'op1': 3,
+        },
+        {
+            'serie': [null, 16],
+            'img': 'img/Nivel1/ejer28.png',
+            'op1': 7,
+        },
+        {
+            'serie': [null, 11],
+            'img': 'img/Nivel1/ejer29.png',
+            'op1': 6,
+        },
+        {
+            'serie': [null, 5],
+            'img': 'img/Nivel1/ejer30.png',
+            'op1': 1,
+        },
+        {
+            'serie': [null, 10],
+            'img': 'img/Nivel1/ejer31.png',
+            'op1': 2,
+        },
+        {
+            'serie': [null, 6],
+            'img': 'img/Nivel1/ejer32.png',
+            'op1': 1,
+        },
+        {
+            'serie': [null, 25],
+            'img': 'img/Nivel1/ejer33.png',
+            'op1': 9,
+        },
+        {
+            'serie': [null, 100],
+            'img': 'img/Nivel1/ejer34.png',
+            'op1': 71,
+        },
+        {
+            'serie': [null, 36],
+            'img': 'img/Nivel1/ejer35.png',
+            'op1': 9,
+        },
+        {
+            'serie': [null, 28],
+            'img': 'img/Nivel1/ejer36.png',
+            'op1': 17,
+        },
+        {
+            'serie': [null, 100],
+            'img': 'img/Nivel1/ejer37.png',
+            'op1': 93,
+        },
+        {
+            'serie': [null, 60],
+            'img': 'img/Nivel1/ejer38.png',
+            'op1': 21,
+        },
+        {
+            'serie': [null, 45],
+            'img': 'img/Nivel1/ejer39.png',
+            'op1': 11,
+        },
+        {
+            'serie': [null, 56],
+            'img': 'img/Nivel1/ejer40.png',
+            'op1': 45,
+        },
+        {
+            'serie': [null, 72],
+            'img': 'img/Nivel1/ejer41.png',
+            'op1': 30,
+        },
+        {
+            'serie': [null, 90],
+            'img': 'img/Nivel1/ejer42.png',
+            'op1': 50,
+        },
+        {
+            'serie': [null, 30],
+            'img': 'img/Nivel1/ejer43.png',
+            'op1': 15,
+        },
+        {
+            'serie': [null, 55],
+            'img': 'img/Nivel1/ejer44.png',
+            'op1': 25,
+        },
+        {
+            'serie': [null, 80],
+            'img': 'img/Nivel1/ejer45.png',
+            'op1': 53,
+        },
+        {
+            'serie': [null, 70],
+            'img': 'img/Nivel1/ejer46.png',
+            'op1': 46,
+        },
+        {
+            'serie': [null, 30],
+            'img': 'img/Nivel1/ejer47.png',
+            'op1': 13,
+        },
+        {
+            'serie': [null, 28],
+            'img': 'img/Nivel1/ejer48.png',
+            'op1': 15,
+        },
+        {
+            'serie': [null, 64],
+            'img': 'img/Nivel1/ejer49.png',
+            'op1': 19,
+        },
+        {
+            'serie': [null, 21],
+            'img': 'img/Nivel1/ejer50.png',
+            'op1': 2,
+        },
+
+       
+    ];
+});
